e5816c77643a2ea04c9168df88ef22b7fcf3a23b
Param Siddharth <paramsiddhartha@gmail.com>
2020-08-15 11:13:00 +0530

    Parse input and plot functions based on user input

--- backmath.py
+++ backmath.py
@@ -1,22 +1,58 @@
 from Equation import Expression
 import numpy as np
+from sys import stderr
 
-def configure(canvas, figure, btn, text):
-	# Define plotting function	
+def configure(canvas, figure, btn, text, limits):	
+	def show_error(prompt=''):
+		print(f'Error: {prompt}', file=stderr)
+
 	def plot_result():
 		# Clear canvas
 		figure.clear()
 		axes = figure.add_subplot(1, 1, 1)
 
+		# Read input
+		fx = read_expression()
+		a, b = read_range()
+		if fx is None:
+			show_error('Invalid expression!')
+			return
+		
+		if a is None or b is None:
+			show_error('Invalid range!')
+			return
+
 		# Calculate
-		x = np.linspace(1, 10, 100)
-		y = x ** 2
-		a, b = 1, 10
+		clarity = 100
+		x = np.linspace(a, b, clarity)
+		try:
+			y = np.vectorize(fx)(x)
+		except:
+			show_error('Invalid expression!')
+			return
 
 		# Draw
 		axes.set_xlim((a, b))
+		axes.axis('on')
 		axes.plot(x, y)
 		canvas.draw()
+
+	def read_expression():
+		expr_raw = text.text()
+		try:
+			expr_eval = Expression(expr_raw, ['x'])
+			return expr_eval
+		except:
+			return None
+
+	def read_range():
+		a_raw, b_raw = limits[0].text(), limits[1].text()
+		try:
+			a_eval = Expression(a_raw)()
+			b_eval = Expression(b_raw)()
+			return a_eval, b_eval
+		except:
+			return 2 * (None,)
 	
 	# Plot on click
 	btn.clicked.connect(plot_result)
--- ui.py
+++ ui.py
@@ -1,6 +1,7 @@
 from PySide2.QtWidgets import \
 	QMainWindow, QWidget, QHBoxLayout, \
-	QVBoxLayout, QPushButton, QTextEdit
+	QVBoxLayout, QPushButton, QLineEdit, \
+	QLabel, QGridLayout, QSizePolicy
 from PySide2.QtCore import Qt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import matplotlib.pyplot as plt
@@ -19,24 +20,67 @@ class MyApp(QMainWindow):
 		# Make interface layouts
 		window = QWidget()
 		layout = QVBoxLayout()
-		top_section = QHBoxLayout()
+		
+		top_section = QVBoxLayout()
+		
+		label_section = QHBoxLayout()
+		range_section = QGridLayout()
 		bottom_section = QVBoxLayout()
 
 		# Create widgets and items
 		figure = plt.figure()
 		canvas = FigureCanvas(figure)
-		expr_input = QTextEdit()
+		
+		label = QLabel(
+			f'''Welcome to {appname}!
+Plot any equation of the form y = f(x).
+
+Enter the function f(x), specify the range of x, and click Plot!''')
+		
+		expr_label = QLabel('f(x) =')
+		expr_input = QLineEdit()
+
+		range_label1 = QLabel('Minimum (x):')
+		range_min = QLineEdit()
+		range_label2 = QLabel('Maximum (x):')
+		range_max = QLineEdit()
+
 		plot_button = QPushButton('Plot')
 
 		# Configure backend
-		backmath.configure(canvas=canvas, figure=figure, btn=plot_button, text=expr_input)
+		backmath.configure(canvas=canvas, figure=figure, btn=plot_button, text=expr_input, limits=(range_min, range_max))
 		
 		# Finalize and display
 		...
 		top_section.addWidget(canvas)
+
+		label_section.addWidget(expr_label)
+		label_section.addWidget(expr_input)
+
+		equally_spaced = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+		equally_spaced.setHorizontalStretch(1)
+
+		range_label1.setSizePolicy(equally_spaced)
+		range_min.setSizePolicy(equally_spaced)
+		range_label2.setSizePolicy(equally_spaced)
+		range_max.setSizePolicy(equally_spaced)
+
+		range_label1.setAlignment(Qt.AlignRight)
+		range_label2.setAlignment(Qt.AlignRight)
+
+		range_section.addWidget(range_label1, 0, 0, 1, 1)
+		range_section.addWidget(range_min, 0, 1, 1, 1)
+		range_section.addWidget(range_label2, 0, 2, 1, 1)
+		range_section.addWidget(range_max, 0, 3, 1, 1)
+		
+		bottom_section.addWidget(label)
+		bottom_section.addLayout(label_section)
+		bottom_section.addLayout(range_section)
 		bottom_section.addWidget(plot_button)
+		
 		layout.addLayout(top_section)
 		layout.addLayout(bottom_section)
+		
 		window.setLayout(layout)
 		self.setCentralWidget(window)
 		self.show()
